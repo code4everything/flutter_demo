c14e299fee111d1477395ac90ed381f81cbe4425
easepan <easepan@qq.com>
2021-12-20 17:50:28 +0800

    feat: add download_button.dart

---
+++ lib/download_button.dart
@@ -0,0 +1,358 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: ExampleCupertinoDownloadButton(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class ExampleCupertinoDownloadButton extends StatefulWidget {
+  const ExampleCupertinoDownloadButton({Key? key}) : super(key: key);
+
+  @override
+  _ExampleCupertinoDownloadButtonState createState() =>
+      _ExampleCupertinoDownloadButtonState();
+}
+
+class _ExampleCupertinoDownloadButtonState
+    extends State<ExampleCupertinoDownloadButton> {
+  late final List<DownloadController> _downloadControllers;
+
+  @override
+  void initState() {
+    super.initState();
+    _downloadControllers = List<DownloadController>.generate(
+        20,
+        (index) => SimulatedDownloadController(onOpenDownload: () {
+              _onOpenDownload(index);
+            }));
+  }
+
+  void _onOpenDownload(int index) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('Open App ${index + 1}')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Apps'),
+      ),
+      body: _buildList(),
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.separated(
+      itemCount: _downloadControllers.length,
+      separatorBuilder: (context, index) => const Divider(),
+      itemBuilder: _buildListItem,
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, int index) {
+    final theme = Theme.of(context);
+    final downloadController = _downloadControllers[index];
+    return ListTile(
+      leading: const DemoAppIcon(),
+      title: Text(
+        'App ${index + 1}',
+        overflow: TextOverflow.ellipsis,
+        style: theme.textTheme.headline6,
+      ),
+      subtitle: Text(
+        'Lorem ipsum dolor #${index + 1}',
+        overflow: TextOverflow.ellipsis,
+        style: theme.textTheme.caption,
+      ),
+      trailing: SizedBox(
+        width: 96.0,
+        child: AnimatedBuilder(
+          animation: downloadController,
+          builder: (context, child) {
+            return DownloadButton(
+              status: downloadController.downloadStatus,
+              downloadProgress: downloadController.progress,
+              onDownload: downloadController.startDownload,
+              onCancel: downloadController.stopDownload,
+              onOpen: downloadController.openDownload,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+enum DownloadStatus {
+  notDownloaded,
+  fetchingDownload,
+  downloading,
+  downloaded,
+}
+
+abstract class DownloadController implements ChangeNotifier {
+  DownloadStatus get downloadStatus;
+
+  double get progress;
+
+  void startDownload();
+
+  void stopDownload();
+
+  void openDownload();
+}
+
+class SimulatedDownloadController extends DownloadController
+    with ChangeNotifier {
+  SimulatedDownloadController({
+    DownloadStatus downloadStatus = DownloadStatus.notDownloaded,
+    double progress = 0,
+    required VoidCallback onOpenDownload,
+  })  : _downloadStatus = downloadStatus,
+        _progress = progress,
+        _onOpenDownload = onOpenDownload;
+
+  DownloadStatus _downloadStatus;
+  double _progress;
+  late final VoidCallback _onOpenDownload;
+  bool _isDownloading = false;
+
+  @override
+  DownloadStatus get downloadStatus => _downloadStatus;
+
+  @override
+  void openDownload() {
+    if (downloadStatus == DownloadStatus.downloaded) {
+      _onOpenDownload();
+    }
+  }
+
+  @override
+  double get progress => _progress;
+
+  @override
+  void startDownload() {
+    if (downloadStatus == DownloadStatus.notDownloaded) {
+      _doSimulatedDownload();
+    }
+  }
+
+  @override
+  void stopDownload() {
+    if (_isDownloading) {
+      _isDownloading = false;
+      _progress = 0;
+      _downloadStatus = DownloadStatus.notDownloaded;
+      notifyListeners();
+    }
+  }
+
+  Future<void> _doSimulatedDownload() async {
+    _isDownloading = true;
+    _downloadStatus = DownloadStatus.fetchingDownload;
+    notifyListeners();
+
+    await Future<void>.delayed(const Duration(seconds: 1));
+
+    if (!_isDownloading) {
+      return;
+    }
+
+    _downloadStatus = DownloadStatus.downloading;
+    notifyListeners();
+    const downloadProgressStops = [0.0, 0.15, 0.45, 0.80, 1.0];
+    for (final stop in downloadProgressStops) {
+      await Future<void>.delayed(const Duration(seconds: 1));
+      if (!_isDownloading) {
+        return;
+      }
+      _progress = stop;
+      notifyListeners();
+    }
+
+    await Future<void>.delayed(const Duration(seconds: 1));
+    if (!_isDownloading) {
+      return;
+    }
+
+    _downloadStatus = DownloadStatus.downloaded;
+    _isDownloading = false;
+    notifyListeners();
+  }
+}
+
+class DemoAppIcon extends StatelessWidget {
+  const DemoAppIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const AspectRatio(
+      aspectRatio: 1.0,
+      child: FittedBox(
+        child: SizedBox(
+          width: 80.0,
+          height: 80.0,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [Colors.red, Colors.blue]),
+                borderRadius: BorderRadius.all(Radius.circular(20.0))),
+            child: Center(
+              child: Icon(
+                Icons.ac_unit,
+                color: Colors.white,
+                size: 40.0,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DownloadButton extends StatelessWidget {
+  const DownloadButton({
+    Key? key,
+    required this.status,
+    this.downloadProgress = 0.0,
+    required this.onDownload,
+    required this.onCancel,
+    required this.onOpen,
+    this.transitionDuration = const Duration(
+      milliseconds: 500,
+    ),
+  }) : super(key: key);
+
+  final DownloadStatus status;
+
+  final double downloadProgress;
+
+  final VoidCallback onDownload;
+
+  final VoidCallback onCancel;
+
+  final VoidCallback onOpen;
+
+  final Duration transitionDuration;
+
+  bool get _isFetching => status == DownloadStatus.fetchingDownload;
+
+  bool get _isDownloading => status == DownloadStatus.downloading;
+
+  bool get _isDownloaded => status == DownloadStatus.downloaded;
+
+  void _onPressed() {
+    switch (status) {
+      case DownloadStatus.notDownloaded:
+        onDownload();
+        break;
+      case DownloadStatus.fetchingDownload:
+        // do nothing.
+        break;
+      case DownloadStatus.downloading:
+        onCancel();
+        break;
+      case DownloadStatus.downloaded:
+        onOpen();
+        break;
+    }
+  }
+
+  Widget _buildButtonShape({required Widget child}) {
+    return AnimatedContainer(
+      duration: transitionDuration,
+      curve: Curves.ease,
+      width: double.infinity,
+      decoration: _isDownloading || _isFetching
+          ? ShapeDecoration(
+              shape: const CircleBorder(), color: Colors.white.withOpacity(0.0))
+          : const ShapeDecoration(
+              shape: StadiumBorder(),
+              color: CupertinoColors.lightBackgroundGray),
+      child: child,
+    );
+  }
+
+  Widget _buildText(BuildContext context) {
+    final text = _isDownloaded ? 'OPEN' : 'GET';
+    final opacity = _isDownloading || _isFetching ? 0.0 : 1.0;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6),
+      child: AnimatedOpacity(
+        duration: transitionDuration,
+        opacity: opacity,
+        curve: Curves.ease,
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.button?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: CupertinoColors.activeBlue,
+              ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDownloadingProgress() {
+    return Positioned.fill(
+      child: AnimatedOpacity(
+        duration: transitionDuration,
+        opacity: _isDownloading || _isFetching ? 1.0 : 0.0,
+        curve: Curves.ease,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            _buildProgressIndicator(),
+            if (_isDownloading)
+              const Icon(
+                Icons.stop,
+                size: 14.0,
+                color: CupertinoColors.activeBlue,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProgressIndicator() {
+    return AspectRatio(
+      aspectRatio: 1.0,
+      child: TweenAnimationBuilder<double>(
+        tween: Tween(begin: 0.0, end: downloadProgress),
+        duration: const Duration(milliseconds: 200),
+        builder: (context, progress, child) {
+          return CircularProgressIndicator(
+            backgroundColor: _isDownloading
+                ? CupertinoColors.lightBackgroundGray
+                : Colors.white.withOpacity(0.0),
+            valueColor: AlwaysStoppedAnimation(_isFetching
+                ? CupertinoColors.lightBackgroundGray
+                : CupertinoColors.activeBlue),
+            strokeWidth: 2.0,
+            value: _isFetching ? null : progress,
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _onPressed,
+      child: Stack(
+        children: [
+          _buildButtonShape(child: _buildText(context)),
+          _buildDownloadingProgress(),
+        ],
+      ),
+    );
+  }
+}
